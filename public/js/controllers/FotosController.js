c449392c8efbaeb6fa9473dd30eba6c60d279dd3
fgabbay <plu_fgabbay@uolinc.com>
2018-01-26 10:22:47 -0200

    Angular - ng-model-options , filter no ng-repeat

--- public/js/controllers/FotosController.js
+++ public/js/controllers/FotosController.js
@@ -1,8 +1,8 @@
 angular.module('alurapic').controller('FotosController',function($scope,$http){
 
     $scope.fotos = [];
-    
-    //$http.get retrna uma promisse, não se pode esquecer
+    $scope.filtro ='';
+
     $http.get('/v1/fotos')
          .success(fotos => $scope.fotos = fotos )
          .error(error => console.log(error));
